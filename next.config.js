9c8f91bc24ccdd4f8aa0f81b3cf108c2d09659cc
Sandesh Salunkhe <sandy.decoder@gmail.com>
2025-05-23 13:56:14 +0530

    Performing cleanup

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    output: 'standalone', // Ensures Netlify builds the correct output
-  }
-  
-  module.exports = nextConfig
-  
